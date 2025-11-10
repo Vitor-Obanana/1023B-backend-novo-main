32de31e1d099d928c8c699f9255e891b5c3befc2
Vitor-Obanana <vitor.andrade@estudante.ifms.edu.br>
2025-11-10 09:00:04 -0300

    Finalização da minha parte do trabalho

--- src/usuarios/usuario.controller.ts
+++ src/usuarios/usuario.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { db } from "../database/banco-mongo.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+
 class UsuarioController {
     async adicionar(req: Request, res: Response) {
         const {nome,idade,email,senha} = req.body
